8ec9be71bd1e5df06c57cdde4b960cfd0cb3ad77
Dylan <gallentesjackdylan16@gmail.com>
2026-07-26 01:51:27 +0800

    debugging

--- scripts/main/credits.gd
+++ scripts/main/credits.gd
@@ -1,4 +1,8 @@
 extends Node2D
 
+func _ready() -> void:
+	ScreenTransition.open()
+
 func _on_back_button_pressed() -> void:
+	await ScreenTransition.close()
 	get_tree().change_scene_to_file("res://scenes/main/Home.tscn")
